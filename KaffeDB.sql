03aeba569fc1300a6dba52273e3a0124554dc313
isabeg <isabeg@stud.ntnu.no>
2022-03-14 16:50:53 +0100

    add auto increment and change in order to ER-diagram

--- KaffeDB.sql
+++ KaffeDB.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Bruker (
-    BrukerID INTEGER NOT NULL,
+    BrukerID INTEGER NOT NULL AUTO_INCREMENT,
     Navn       VARCHAR(30) NOT NULL,
     Epost      VARCHAR(50) NOT NULL,
     Passord    VARCHAR(30) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE Kaffebrenneri (
     CONSTRAINT Kaffebrenneri_PK PRIMARY KEY (BrenneriID));
 
 CREATE TABLE Kaffegård(
-    GårdID INTEGER NOT NULL,
+    GårdID INTEGER NOT NULL AUTO_INCREMENT,
     Navn     VARCHAR(30) NOT NULL,
     Land     VARCHAR(30) NOT NULL,
     Region   VARCHAR(30) NOT NULL,
@@ -19,14 +19,14 @@ CREATE TABLE Kaffegård(
 );
 
 CREATE TABLE Foredlingsmetode(
-    ForedlingID INTEGER NOT NULL,
+    ForedlingID INTEGER NOT NULL AUTO_INCREMENT,
     Navn     VARCHAR(30) NOT NULL,
     Beskrivelse VARCHAR(100),
     CONSTRAINT Foredlingsmetode_PK PRIMARY KEY (ForedlingID)
 );
 
 CREATE TABLE  Kaffeparti (
-    PartiID       INTEGER NOT NULL,
+    PartiID       INTEGER NOT NULL AUTO_INCREMENT,
     InnhøstingsÅr INTEGER NOT NULL,
     Kilopris      INTEGER NOT NULL,
     GårdsID       INTEGER NOT NULL,
@@ -42,40 +42,40 @@ CREATE TABLE  Kaffeparti (
 
 CREATE TABLE Kaffe (
   KaffeNavn     VARCHAR(30) NOT NULL,
-  Navn          VARCHAR(30) NOT NULL,
+  BrenneriNavn  VARCHAR(30) NOT NULL,
   Beskrivelse   VARCHAR(100),
   Brenningsgrad VARCHAR(10) CHECK (Brenningsgrad = 'lys' OR Brenningsgrad = 'middels' OR Brenningsgrad = 'mørk'),
   BrenningDato DATE NOT NULL,
   Kilopris      INTEGER NOT NULL,
-  BrenneriID    INTEGER NOT NULL,
   PartiID       INTEGER NOT NULL,
-  CONSTRAINT Kaffe_PK PRIMARY KEY (KaffeNavn, BrenneriNavn),
-  CONSTRAINT Kaffe_FK1 FOREIGN KEY (BrenneriID) REFERENCES Kaffebrenneri(BrenneriNavn)
+  CONSTRAINT Kaffe_FK1 FOREIGN KEY (BrenneriNavn) REFERENCES Kaffebrenneri(BrenneriNavn)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   CONSTRAINT Kaffe_FK2 FOREIGN KEY (PartiID) REFERENCES Kaffeparti(PartiID)
     ON UPDATE CASCADE
-    ON DELETE CASCADE);
+    ON DELETE CASCADE,
+    CONSTRAINT Kaffe_PK PRIMARY KEY (KaffeNavn, BrenneriNavn)
+  );
 
 CREATE TABLE Kaffesmaking (
-  SmakID      INTEGER NOT NULL,
+  SmakID      INTEGER NOT NULL AUTO_INCREMENT,
   BrukerID    INTEGER NOT NULL,
-  KaffeNavn   VARCHAR(30) NOT NULL,
   Beskrivelse VARCHAR(100),
   Poeng INTEGER CHECK (Poeng >= 0 AND Poeng <= 10),
   Dato DATE NOT NULL,
-  CONSTRAINT Kaffesmaking_PK PRIMARY KEY (SmakID, BrukerID),
+  KaffeNavn   VARCHAR(30) NOT NULL,
   CONSTRAINT Kaffesmaking_FK1 FOREIGN KEY (BrukerID) REFERENCES Bruker(BrukerID)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   CONSTRAINT Kaffesmaking_FK2 FOREIGN KEY (KaffeNavn) REFERENCES Kaffe(KaffeNavn)
     ON UPDATE CASCADE
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT Kaffesmaking_PK PRIMARY KEY (SmakID, BrukerID),
 );
 
 
 CREATE TABLE Kaffebønne(
-    BønneID INTEGER NOT NULL,
+    BønneID INTEGER NOT NULL AUTO_INCREMENT,
     Artsnavn     VARCHAR(30) NOT NULL,
     CONSTRAINT Kaffegård_PK PRIMARY KEY (BønneID)
 );
